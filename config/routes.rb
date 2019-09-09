269ba6587012e6d2755985ff1ca882a0eab981f9
ansaryergesh <yergeshovansar@gmail.com>
2019-09-10 02:09:21 +0600

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do 
-    root 'application#hello' 
-end
+#    root 'static_pages#home' 
+    get 'static_pages/help'
+    get 'static_pages/about'
+    get 'static_pages/home'
+end 
