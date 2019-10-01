fb3786e740264ed46f0ee693637468b2ed54e20f
Ansar <yergeshovansar@gmail.com>
2019-10-02 00:53:46 +0600

    changes

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,17 +16,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.<CSS class for field with error>'
   end
 
-  # test "valid signup information" do
-  #   get signup_path
-  #   assert_difference 'User.count', 1 do
-  #     post users_path, params: { user: { name: "Example User",
-  #                                email: "user@example.com",
-  #                                password: "password",
-  #                                password_confirmation: "password"
-  #   } }
-  #   end
-  #   follow_redirect!
-  #   assert_template 'users/show'
-  #   assert_not flash.FILL_IN
-  # end
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                 email: "user@example.com",
+                                 password: "password",
+                                 password_confirmation: "password"
+    } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.FILL_IN
+  end
 end
