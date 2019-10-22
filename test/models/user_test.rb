77933f8a3ba9a847b45533f6be70e9df8951b6a9
Ansar <yergeshovansar@gmail.com>
2019-10-23 01:33:39 +0600

    add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -80,4 +80,13 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)assert michael.following?(archer)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
 end
